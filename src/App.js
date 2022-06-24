e7968178b0733603b3987e80d63bf246bcda58f0
Damian-luis <damian.luis.porta@gmail.com>
2022-06-23 21:07:01 -0300

    everithing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useState,useEffect } from 'react';
 function App() {
+const [datos,setDatos]=useState();
+ async function traer(){  
+  const data = await axios.get("https://gorest.co.in/public/v2/users").then(e=>{setDatos(e.data)}) 
+
+}
+
+
+useEffect(()=>{
+  traer()
+},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav><p>No mucho que ver aqui, simple fetch a una api con react ;)</p></nav>
+
+
+      <div className="body">
+      {datos? datos.map(e=>{return <div className="card" key={e.id}> 
+      
+      <h4 key={e.id}>Nombre del usuario:</h4> <p>{e.name}</p><br/>
+      <h4 key={e.id}>Mail de contacto del usuario:</h4><p>{e.email}</p><br/>
+      <h4 key={e.id}>Genero del usuario:</h4><p>{e.gender}</p><br/>
+      <h4 key={e.id}>Estado del usuario:</h4> <p>{e.status}</p>
+
+      </div>}):<p>Loading</p>}
+      </div>
+
+
+
+    <footer><h3>Fin de la pagina</h3></footer>
+
     </div>
   );
 }
